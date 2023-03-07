582636be24a99e5d0f7cf80ea67a4f7b1b8e5a39
geodevcode <geodevinstitute@gmail.com>
2023-03-07 14:33:42 +0100

    footerSection.vue component created

---
+++ src/components/footerSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="copywrite">
+    <p>©️2023 Altschooler ---- Rasheed Olatunde</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.copywrite {
+  height: 10vh;
+  background: #4a9edb;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+}
+</style>
