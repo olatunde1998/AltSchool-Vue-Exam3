ab68ebea3f4ec131d45a50694d5b2224c5a6f5fb
geodevcode <geodevinstitute@gmail.com>
2023-03-08 00:47:41 +0100

    styling added

--- src/components/resetCounter.vue
+++ src/components/resetCounter.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-    <button @click='reset'>Reset</button>
+    <button class='reset' @click='reset'>Reset</button>
     </div>
 </template>
 <script>
@@ -12,5 +12,23 @@ export default{
     }
   }
 </script>
+
+<style>
+.reset{
+  padding: 12px;
+  border-radius: 5px;
+   background: #4a9edb;
+  border: 1px solid ;
+  color: white;
+  width: 70px;
+  font-size: 16px;
+}
+.reset:hover{
+  background: white;
+  color: #4a9edb;
+  border: 1px solid #4a9edb;
+  border-radius: 10px;
+}
+</style>
 
 
