210a9396edf8cc0c5d202b2fec64c96d043a0564
geodevcode <geodevinstitute@gmail.com>
2023-03-07 14:34:08 +0100

    projectInfo.vue component created

---
+++ src/components/projectInfo.vue
@@ -0,0 +1,19 @@
+<template lang="">
+<h3 class='project'>Project 👎</h3>
+    <div class="project-info">
+        <p>Github link</p>
+        <p>Hosting/Live link</p>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style>
+.project-info{
+    padding-left:10px;
+}
+.project{
+        color: white;
+    }
+    
+</style>
