ed35baed587e554ad042f8c73e370e607b47b9a9
geodevcode <geodevinstitute@gmail.com>
2023-03-07 14:34:51 +0100

    skillsUsed.vue component created

---
+++ src/components/skillsUsed.vue
@@ -0,0 +1,69 @@
+<template lang="">
+  <h3 class='technology-used'>Techonlogy used</h3>
+  <div class="skill-box">
+    <div class="html-box">
+      <div class="html-img">
+        <img src="@/assets/html.png" alt="" width="50" height="50" />
+      </div>
+      <p>HTML</p>
+    </div>
+    <div class="html-box">
+      <div class="html-img">
+        <img src="@/assets/css.png" alt="" width="50" height="50" />
+      </div>
+      <p>CSS</p>
+    </div>
+    <div class="html-box">
+      <div class="html-img">
+        <img src="@/assets/vuejs-logo.png" alt="" width="50" height="50" />
+      </div>
+      <p>VueJs</p>
+    </div>
+    <div class="html-box">
+      <div class="html-img">
+        <img src="@/assets/vuex.jpg" alt="" width="50" height="50" />
+      </div>
+      <p>Vuex</p>
+    </div>
+    <div class="html-box">
+      <div class="html-img">
+        <img src="@/assets/vercel.jpg" alt="" width="100" height="50" />
+      </div>
+      <p>Vercel</p>
+    </div>
+    <div class="html-box">
+      <div class="html-img">
+        <img src="@/assets/git-github.png" alt="" width="100" height="50" />
+      </div>
+      <p>Git & Github</p>
+    </div>
+  </div>
+</template>
+<script></script>
+<style>
+.skill-box {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.html-box {
+  border: 1px solid #4a9edb;
+  width: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+.html-img{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.technology-used{
+  color: #4a9edb;
+
+}
+</style>
