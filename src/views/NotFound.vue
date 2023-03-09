072e575a18be0efbfd8b0c1d7afd8ad6c1d12fb8
geodevcode <geodevinstitute@gmail.com>
2023-03-09 16:11:10 +0100

    html element added and neccessary styling added

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,38 @@
 <template>
-    <p> 404</p>
-    <p>page not found </p>
-    <router-link to='/'>Back to home</router-link>
-
-
+  <div class="notfound-wrapper">
+    <div class="notfound-box">
+      <p class="text404">404</p>
+      <p >page not found</p>
+      <router-link to="/" class="back">Back to home</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default{
-
-    }
+export default {};
 </script>
 
+<style>
+.notfound-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.notfound-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px;
+  margin: 100px auto;
+}
+.back{
+    text-decoration: none;
+    cursor: pointer;
+    margin-top: 20px;
+}
+.text404{
+    font-size: 24px;
+    margin-bottom: 0px;
+
+}
+</style>
