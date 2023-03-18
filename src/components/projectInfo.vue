2a9e6b0d7407e4e980dd48cf485a0591992f16e7
geodevcode <geodevinstitute@gmail.com>
2023-03-18 11:11:59 +0100

    paragraph elements were changed to unordered list elements

--- src/components/projectInfo.vue
+++ src/components/projectInfo.vue
@@ -1,28 +1,27 @@
 <template lang="">
-<h3 class='project'>Project 👎</h3>
-    <div class="project-info">
-        <p><a href="https://github.com/olatunde1998/AltSchool-Vue-Exam3">Github</a></p>
-        <p><a href="https://altschool-vue-exam-3.vercel.app/">Hosting/Live link</a></p>
-
-    </div>
+  <h3 class="project">Project 👎</h3>
+  <ul class="project-info">
+    <li>
+      <a href="https://github.com/olatunde1998/AltSchool-Vue-Exam3">Github</a>
+    </li>
+    <li>
+      <a href="https://altschool-vue-exam-3.vercel.app/">Hosting/Live link</a>
+    </li>
+  </ul>
 </template>
-<script setup>
-
-</script>
+<script setup></script>
 <style>
-.project-info{
-    padding-left:10px;
-    color: rgb(81, 71, 71);
+.project-info {
+  padding-left: 10px;
+  color: rgb(81, 71, 71);
   font-weight: 200;
 }
-.project{
-        color: white;
-    }
-    a{
-        text-decoration: none;
-    color: rgb(81, 71, 71);
-    cursor: pointer;
-
-    }
-    
+.project {
+  color: white;
+}
+a {
+  text-decoration: none;
+  color: rgb(81, 71, 71);
+  cursor: pointer;
+}
 </style>
